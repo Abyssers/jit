8f9d70d8a3013dfb80db5db8ad520f08636a1371
Morilence <1280659615@qq.com>
2022-08-29 17:47:37 +0800

    build: added lint 4 ts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@
  * configuration: https://github.com/okonet/lint-staged#Configuration
  */
 module.exports = {
-    "*.js": ["eslint --fix", "prettier --write"],
+    "*.{js,ts}": ["eslint --fix", "prettier --write"],
     "*.{json,jsonc}": ["prettier --write"],
     "*.{yml,yaml}": ["prettier --write"],
     "*.{md,markdown}": ["markdownlint-cli2"],
