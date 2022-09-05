5cf45507a96fd1dcf982803c999be95c3dacd914
Morilence <1280659615@qq.com>
2022-09-05 20:23:05 +0800

    build: made rollup to adapt typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
 const json = require("@rollup/plugin-json");
-const eslint = require("@rollup/plugin-eslint");
 const typescript = require("@rollup/plugin-typescript");
 const strip = require("@rollup/plugin-strip");
 
 module.exports = {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: {
-        dir: "dist/index.js",
+        file: "dist/index.js",
         format: "cjs",
     },
-    plugins: [json(), eslint(), typescript(), strip()],
+    plugins: [json(), typescript(), strip({ debugger: true })],
 };
