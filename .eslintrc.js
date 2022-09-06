07d367b84d33e6f0866ecca1f304e76219b87bff
Morilence <1280659615@qq.com>
2022-09-06 20:10:28 +0800

    build: allowed explicit any

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,6 @@ module.exports = {
 
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
     },
 };
