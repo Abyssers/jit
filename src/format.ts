ec0784f2c22a2462107f88659658681207102da2
Morilence <1280659615@qq.com>
2022-09-07 17:54:14 +0800

    perf: replenished logic of the log-formatting

--- src/format.ts
+++ src/format.ts
@@ -14,21 +14,54 @@ export const formatters: { [key: string]: Formatter } = {
         ...params: string[]
     ): ReturnType<Formatter> => {
         let formatted: { [key: string]: any } | { [key: string]: any }[];
-        if (args.length === 0 || Array.prototype.includes.call(args, "--pretty=medium")) {
+        if ((args as GitCommandArg[]).includes("--oneline") || (args as GitCommandArg[]).includes("--pretty=oneline")) {
+            // ...
+        } else if ((args as GitCommandArg[]).includes("--pretty=reference")) {
+            // ...
+        } else {
             formatted = stdout
                 .split("commit ")
                 .filter(item => item !== "")
                 .map(text => {
-                    const [name, email] = /Author: \S+ <\S+@\S+\.\S+>/
-                        .exec(text)[0]
-                        .replace(/(Author: |<|>)/g, "")
-                        .split(" ");
-                    return {
+                    const commit = {
                         hash: /[a-z0-9]+/.exec(text)[0],
-                        author: { name, email },
-                        authorDate: new Date(/Date: {3}.+/.exec(text)[0].replace(/Date: {3}/, "")),
                         titleLine: /\n{2} {4}.+/.exec(text)[0].replace(/\n{2} {4}/, ""),
+                        ...(/\nMerge: [a-z0-9]+ [a-z0-9]+\n/.test(text)
+                            ? {
+                                  merge: /Merge: [a-z0-9]+ [a-z0-9]+/
+                                      .exec(text)[0]
+                                      .replace(/Merge: /, "")
+                                      .split(" "),
+                              }
+                            : {}),
+                        ...(/\n(Author)?Date: +.+\n/.test(text)
+                            ? {
+                                  authorDate: new Date(
+                                      /(Author)?Date: +.+/.exec(text)[0].replace(/(Author)?Date: +/, "")
+                                  ),
+                              }
+                            : {}),
+                        ...(/\nCommitDate: +.+\n/.test(text)
+                            ? {
+                                  committerDate: new Date(/CommitDate: +.+/.exec(text)[0].replace(/CommitDate: +/, "")),
+                              }
+                            : {}),
                     };
+                    if (/\nAuthor: +\S+ <\S+@\S+\.\S+>\n/.test(text)) {
+                        const [name, email] = /Author: +\S+ <\S+@\S+\.\S+>/
+                            .exec(text)[0]
+                            .replace(/(Author: +|<|>)/g, "")
+                            .split(" ");
+                        Object.assign(commit, { author: { name, email } });
+                    }
+                    if (/\nCommit: +\S+ <\S+@\S+\.\S+>\n/.test(text)) {
+                        const [name, email] = /Commit: +\S+ <\S+@\S+\.\S+>/
+                            .exec(text)[0]
+                            .replace(/(Commit: +|<|>)/g, "")
+                            .split(" ");
+                        Object.assign(commit, { committer: { name, email } });
+                    }
+                    return commit;
                 });
         }
         return formatted;
